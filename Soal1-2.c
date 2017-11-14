f18b08ccfdd0d2261169641bb5dc88bb3680343e
DanielDeka <dekaslr8@gmail.com>
2017-11-14 19:19:24 +0700

    Update Soal1-2.c
    Tambah fungsi xmp_getattr

--- Soal1-2.c
+++ Soal1-2.c
@@ -21,3 +21,20 @@
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
 #endif
+
+static const char *dirpath = "/home/daniel/Downloads";
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+	int res;
+	car fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath,stbuf);
+
+	if(res == -1)
+	{
+		return -errno;
+	}
+
+	return 0;
+}
